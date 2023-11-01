a8909e5b643d1cd01fd847a5ba0a3de3b489ef69
gvek <49954788+gvek@users.noreply.github.com>
2023-11-01 20:42:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,19 +8,63 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+// const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS_TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+
+    const getPetsURL = 'https://api.hubspot.com/crm/v3/objects/pets/?properties=pet_name&properties=pet_type&properties=breed';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(getPetsURL, { headers });
+        let data = resp.data.results;
+        data = data.sort((a, b) => parseInt(a.properties.pet_type) - parseInt(b.properties.pet_type));
+
+        res.render('homepage', { title: 'Pets Table', data });
+    } catch (error) {
+        console.error(error);
+    }
+
+});
+
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+app.get('/update-cobj', async (req, res) => {
+    res.render('update', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum'});
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {
+    const update = {
+        properties: {
+            "pet_type": req.body.pet_type,
+            "pet_name": req.body.pet_name,
+            "pet_type": req.body.pet_type
+        }
+    }
+
+    const updatePetsURL = 'https://api.hubapi.com/crm/v3/objects/pets';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        await axios.post(updatePetsURL, update, { headers } );
+        res.redirect('/');
+    } catch(err) {
+        console.error(err);
+    }
+});
+// Localhost
+app.listen(3000, () => console.log('Listening on http://localhost:3000'));
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
